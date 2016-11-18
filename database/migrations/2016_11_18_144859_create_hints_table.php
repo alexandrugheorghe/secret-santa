340efcce3bc88c3563030eac94a40a419709cdc9
Jakub Giminski <jakubgiminski@gmail.com>
2016-11-18 15:11:01 +0000

    Adds db migrations

---
+++ database/migrations/2016_11_18_144859_create_hints_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hints', function (Blueprint $table) {
+            $table->string('receiver_id');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hints');
+    }
+}
