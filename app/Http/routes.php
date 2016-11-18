c2d511e27a2c4dc1ec92d5aaa9122af1289c23b0
Alexandru Gheorghe <alexandru.gheorghe@lifeworks.com>
2016-11-18 19:03:26 +0000

    Added action on feature key enable.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,5 +2,6 @@
 
 Route::group(['middleware' => 'cors'], function(\Illuminate\Routing\Router $router){
     $router->get('/hints', 'HintController@getDummies');
+    $router->post('/users', 'UserController@postRaffle');
     $router->get('/dev/hints', 'HintController@index');
 });
