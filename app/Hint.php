e0379ebcc0ef9ded53ea60090396b91ee3a34929
vagrant <vagrant@secret-santa>
2016-11-18 18:51:56 +0000

    Modifies Hint model to cast carbon to timestamps

--- app/Hint.php
+++ app/Hint.php
@@ -9,4 +9,9 @@ class Hint extends Model
     public $primaryKey = 'receiver_id';
 
     public $incrementing = false;
+
+    protected $casts = [
+        'created_at' => 'timestamp',
+        'updated_at' => 'timestamp',
+    ];
 }
