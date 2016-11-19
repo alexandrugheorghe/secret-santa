16f997f5c469559ced57a36f9345259813dccaac
Jakub Giminski <jakubgiminski@gmail.com>
2016-11-19 22:44:12 +0000

    Refactors HintsTableSeeder

--- database/seeds/HintsTableSeeder.php
+++ database/seeds/HintsTableSeeder.php
@@ -16,35 +16,35 @@ class HintsTableSeeder extends Seeder
 
         DB::table('hints')->insert([
             'receiver_id' => '5811f279fc1bc7db15d9af5f',
-            'content' => $faker->realText($maxNbChars = 120, $indexSize = 2),
+            'content' => $faker->realText(120),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
 
         DB::table('hints')->insert([
             'receiver_id' => '5811f279fc1bc7db15d9af5f',
-            'content' => $faker->realText($maxNbChars = 120, $indexSize = 2),
+            'content' => $faker->realText(120),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
 
         DB::table('hints')->insert([
             'receiver_id' => '002',
-            'content' => $faker->realText($maxNbChars = 120, $indexSize = 2),
+            'content' => $faker->realText(120),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
 
         DB::table('hints')->insert([
             'receiver_id' => '002',
-            'content' => $faker->realText($maxNbChars = 120, $indexSize = 2),
+            'content' => $faker->realText(120),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
 
         DB::table('hints')->insert([
             'receiver_id' => '002',
-            'content' => $faker->realText($maxNbChars = 120, $indexSize = 2),
+            'content' => $faker->realText(120),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
