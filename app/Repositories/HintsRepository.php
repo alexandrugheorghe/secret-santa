a5495b0f4304a39a7ec8b5a4f3488e18667ea5de
Jakub Giminski <jakubgiminski@gmail.com>
2016-11-20 16:28:36 +0000

    Adds save methods to HintsRepository

--- app/Repositories/HintsRepository.php
+++ app/Repositories/HintsRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Models\Hint;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 
 class HintsRepository
 {
@@ -29,6 +30,25 @@ class HintsRepository
             ->where('revealed_at', '<', Carbon::now())
             ->get(['content', 'created_at']);
 
+        if ($result->isEmpty()) {
+            throw new RepositoryException('Result is empty');
+        }
+
         return $result;
+    }
+
+    /**
+     * @param Hint $hint
+     *
+     * @return bool
+     */
+    public function save(Hint $hint)
+    {
+        return $hint->save();
+    }
+
+    public function saveCollection(Collection $hints)
+    {
+        return $this->model->insert($hints->toArray());
     }
 }
