d5de4185f6d5effb2f2ef432408338a5dc46e3fc
Jakub Giminski <jakubgiminski@gmail.com>
2016-11-20 16:27:10 +0000

    Adds setters to Hint model

--- app/Models/Hint.php
+++ app/Models/Hint.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\ValueObjects\HintType;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Hint extends Model
@@ -15,9 +17,30 @@ class Hint extends Model
         'updated_at' => 'timestamp',
     ];
 
+    public function setReceiverId(string $receiverId) : self
+    {
+        $this->receiver_id = $receiverId;
+
+        return $this;
+    }
+
     public function setContent(string $content) : self
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function setType(HintType $type) : self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function setRevealedAt(Carbon $revealedAt) : self
+    {
+        $this->revealed_at = $revealedAt;
 
         return $this;
     }
