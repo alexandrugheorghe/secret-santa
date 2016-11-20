976d7d5d454f49bba839d533b695a105867e312e
Jakub Giminski <jakubgiminski@gmail.com>
2016-11-20 14:37:49 +0000

    Adds reveal_at values to the seeder

--- database/seeds/HintsTableSeeder.php
+++ database/seeds/HintsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\ValueObjects\HintType;
+use Carbon\Carbon;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
 
@@ -19,6 +20,7 @@ class HintsTableSeeder extends Seeder
             'receiver_id' => '5811f279fc1bc7db15d9af5f',
             'content' => 'Your Secret Santa is female',
             'type' => HintType::gender(),
+            'revealed_at' => Carbon::now(),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
@@ -27,6 +29,7 @@ class HintsTableSeeder extends Seeder
             'receiver_id' => '5811f279fc1bc7db15d9af5f',
             'content' => 'Your Secret Santa joined in March',
             'type' => HintType::joinedInMonth(),
+            'revealed_at' => Carbon::now()->addDay(),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
@@ -35,6 +38,7 @@ class HintsTableSeeder extends Seeder
             'receiver_id' => '002',
             'content' => 'Your Secret Santa joined in 2015',
             'type' => HintType::joinedInYear(),
+            'revealed_at' => Carbon::now()->subHour(),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
@@ -43,6 +47,7 @@ class HintsTableSeeder extends Seeder
             'receiver_id' => '002',
             'content' => 'Your Secret Santa\s birthday is on May 31, 1989',
             'type' => HintType::birthday(),
+            'revealed_at' => Carbon::now()->subWeek(),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
@@ -51,6 +56,7 @@ class HintsTableSeeder extends Seeder
             'receiver_id' => '002',
             'content' => 'Your Secret Santa\'s last name starts with G',
             'type' => HintType::lastNameStartsWith(),
+            'revealed_at' => Carbon::now()->addWeek(),
             'created_at' => $faker->dateTime,
             'updated_at' => $faker->dateTime,
         ]);
